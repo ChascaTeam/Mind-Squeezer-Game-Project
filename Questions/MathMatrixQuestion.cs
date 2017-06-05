f7ae35e4c32cda63b2937a280fdc0e220f734bd8
Konstantin Lupov <arebando10@abv.bg>
2017-06-06 00:04:40 +0300

    Work on MathMatrix

--- Questions/MathMatrixQuestion.cs
+++ Questions/MathMatrixQuestion.cs
@@ -9,6 +9,10 @@ namespace MindSqueezer.Questions
 {
     public class MathMatrixQuestion : Question
     {
+        private const int rows = 6;
+        private const int cols = 6;
+        private int[][] matrix = new int[rows][];
+        private List<string> correctAnswers = new List<string>();
         public MathMatrixQuestion(string equation)
         {
             this.QuestionText = String.Format(Messages.MathMatrixEquationCoordinates, equation);
@@ -28,39 +32,95 @@ namespace MindSqueezer.Questions
                 "10*0 + 9", "7 - 5 + 2", "2 * 3 + 1", "2 * 4 - 2", "12 - 7", "13 - 9", "9 - 8 + 2 + 3 + 1", "6 + 1 - 7"
             };
 
-            //Matrix initialization
-            int rows = 6;
-            int cols = 6;
-            int[][] matrix = new int[rows][];
+            //Matrix initialization.
+            InitializeMatrix();
 
-            for (int row = 0; row < rows; row++)
-            {
-                matrix[row] = new int[cols];
-            }
-
-            //Choosing an equation
-            
+            //Choosing a random equation.
             int equationIndex = RandomGenerator.GetRandomNumber(equationsArr.Length);
 
             string equation = equationsArr[equationIndex];
 
-            //Print the equation
-            Console.WriteLine($"{equation} = X");
+            //Printing the equation.
+            PrintEquation(equation);
 
-            //Equations calculator
+            //Calculating the equation.
             int result = CalculateEquation(equation);
+            
+            //Populating the matrix containing at least one occurrence of the result.
+            PopulateMatrix(result);
+
+            //Printing the matrix.
+            PrintMatrix(matrix);
+
+           
+        }
+
+        private void InitializeMatrix()
+        {
+            for (int rowIndex = 0; rowIndex < rows; rowIndex++)
+            {
+                matrix[rowIndex] = new int[cols];
+            }
+        }
+
+        private void PopulateMatrix(int result)
+        {
+            int row = RandomGenerator.GetRandomNumber(matrix.Length);
+            int col = RandomGenerator.GetRandomNumber(matrix[0].Length);
+
+            matrix[row][col] = result;
+            correctAnswers.Add($"{(char)(row+65)} {col+1}");
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
+                {
+                    if (rowIndex != row && colIndex != col)
+                    {
+                        int numToAdd = RandomGenerator.GetRandomNumber(10);
+                        matrix[rowIndex][colIndex] = numToAdd;
+                        if (numToAdd == result)
+                        {
+                            correctAnswers.Add($"{(char)(rowIndex + 65)} {colIndex + 1}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void PrintEquation(string equation)
+        {
+            Writer.WriteMessageOnNewLine($"{equation} = X");
+            Writer.WriteMessageOnNewLine("Example answer: A 1");
         }
 
         private static int CalculateEquation(string equation)
         {
             DataTable dt = new DataTable();
-            var v = (int)dt.Compute(equation, "");
+            var v = int.Parse(dt.Compute(equation, "").ToString());
             return v;
         }
 
         public override bool IsCorrectAnswer(string answer)
         {
-            return this.Answer.ToLower().Equals(answer.ToLower()) && answer.Length != 0;
+            return this.correctAnswers.Contains(answer) && answer.Length != 0;
+        }
+
+        private static void PrintMatrix(int[][] matrix)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("- 1 2 3 4 5 6");
+            for (int rowIndex = 0; rowIndex < matrix.Length; rowIndex++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write((char)(rowIndex + 65));
+                for (int colIndex = 0; colIndex < matrix[0].Length; colIndex++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(" " + matrix[rowIndex][colIndex]);
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
         }
     }
 }
