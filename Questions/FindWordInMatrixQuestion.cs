f8b2c4f8c63711c99c9fcc0446d811851e36bd09
DDimitrova90 <ddimitrova90@gmail.com>
2017-06-05 15:32:07 +0300

    Added Intializing Matrix in FindWordInMatrixQuestion.cs

--- Questions/FindWordInMatrixQuestion.cs
+++ Questions/FindWordInMatrixQuestion.cs
@@ -21,7 +21,40 @@
 
         public override void GenerateQuestion()
         {
-            throw new NotImplementedException();
+            Console.WriteLine();
+
+            // Array with 4 and 5 letters words
+            string[] words = new string[]
+            {
+                "quake", "glazy", "japan", "joker", "pizza", "quick", "zombi",
+                "quirk", "jazz", "quiz", "jump", "junk", "cozy", "joke", "java",
+                "lazy", "maze", "zoom", "kick", "haze", "mock", "back", "wick",
+                "judo", "pump", "exam", "bump", "zone", "pack", "doze", "quit",
+                "juice", "knock", "prize", "klick", "hazel", "query", "zebra",
+                "chump", "mummy", "check", "chick", "puppy", "eject", "enjoy", "glaze"
+            };
+
+            // Array with alphabet
+            char[] alphabet = new char[]
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            };
+
+            // Initializing of matix
+            int rows = 4;
+            int cols = 5;
+            char[][] matrix = new char[rows][];
+
+            for (int currRow = 0; currRow < 4; currRow++)
+            {
+                matrix[currRow] = new char[cols];
+            }
+
+            // Choosing random word from the array
+            Random random = new Random();
+            int wordIndex = random.Next(0, words.Length - 1);
+            int count = 0;
         }
 
         public override bool IsCorrectAnswer(string answer)
