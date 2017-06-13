0aa3ed138606122bc2897d989716ea20f4479f9c
VaskoViktorov <vaskoviktorov@gmail.com>
2017-06-13 20:23:10 +0300

    Missing class added

---
+++ MindSqueezer/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MindSqueezer
+{
+    class Timer
+    {
+        private static Thread inputThread;
+        private static AutoResetEvent getInput, gotInput;
+        private static string input;
+
+        private static void reader()
+        {
+            while (true)
+            {
+                getInput.WaitOne();
+                input = Console.ReadLine();
+                gotInput.Set();
+            }
+        }
+
+        
+        public static bool TryReadLine(out string line, int timeOutMillisecs = Timeout.Infinite)
+        {
+            getInput = new AutoResetEvent(false);
+            gotInput = new AutoResetEvent(false);
+            inputThread = new Thread(reader);
+            inputThread.IsBackground = true;
+            inputThread.Start();
+            getInput.Set();
+            bool success = gotInput.WaitOne(timeOutMillisecs);
+            if (success)
+            {
+                line = input;
+                inputThread.Abort();
+            }
+            else
+            {
+                line = null;
+                inputThread.Abort();
+            }               
+            return success;
+        }
+
+    }
+}
